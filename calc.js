eba22fdec55e16975c3510865b3ae9c6a4396de6
luvrur <155270349+luvrur@users.noreply.github.com>
2024-02-22 16:55:12 +0900

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,9 +1,7 @@
-//変数(num)に1以上の正の数を代入
 let num = Math.floor(Math.random() * 100);
-// 変数numの値を出力する
 console.log(num);
 
-// 変数(num)が3の倍数の場合“3の倍数です”という文字列を出力する
+
 if (num  % 3 === 0) {
     console.log("3の倍数です");
 }else if(num % 5 === 0) {
